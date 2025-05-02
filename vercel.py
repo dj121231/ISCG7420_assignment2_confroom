a713191f8741f1033cb34f22c8ec60f0d896d11f
bluetree <63972280+dj121231@users.noreply.github.com>
2025-05-03 06:57:32 +1200

    🐛 Fix vercel.py: expose app instead of application

--- vercel.py
+++ vercel.py
@@ -1,4 +1,4 @@
 import os
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conference.settings')
-application = get_wsgi_application() 
+app = get_wsgi_application()
